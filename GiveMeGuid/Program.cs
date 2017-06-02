58ea188362214a8a327a6b3e75f72af4617376d4
DavidThompsonZA <david@parcelninja.com>
2017-06-02 09:32:03 +0200

    initial commit

---
+++ GiveMeGuid/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiveMeGuid
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(Guid.NewGuid().ToString());
+        }
+    }
+}
